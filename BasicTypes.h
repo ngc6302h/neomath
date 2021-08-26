eb33c3895144a678d3ff616ed4d5cb49bd5e2beb
ngc6302h <shortanemoia@protonmail.com>
2021-08-26 14:05:37 +0200

    Meta: add type guard
    For projects in which both neo and neomath are included, and in order to make
    neomath independent of neo, let's define types in both libraries, with safeguards
    to avoid type redefinition.

---
+++ BasicTypes.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef NEO_BASIC_TYPES
+#define NEO_BASIC_TYPES
+
+using i8 = __INT8_TYPE__;
+using u8 = __UINT8_TYPE__;
+using i16 = __INT16_TYPE__;
+using u16 = __UINT16_TYPE__;
+using i32 = __INT32_TYPE__;
+using u32 = __UINT32_TYPE__;
+using i64 = __INT64_TYPE__;
+using u64 = __UINT64_TYPE__;
+
+using f32 = float;
+using f64 = double;
+
+using int8_t = i8;
+using uint8_t = u8;
+using int16_t = i16;
+using uint16_t = u16;
+using int32_t = i32;
+using uint32_t = u32;
+using int64_t = i64;
+using uint64_t = u64;
+
+using size_t = __SIZE_TYPE__;
+#endif
